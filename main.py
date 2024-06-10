39abe3f76cba376efcfb8f71a3270032ab43c927
M.Adli1 <adlimuhammad847@gmail.com>
2024-06-10 15:01:23 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+
+1. Sekuensial: Langkah berurutan
+2. Percabangan: Langkah melompat jika kondisi terpenuhi
+3. Perulangan: Mengulang langkah yang sama berkali kali selama/ sampai kondisi terpenuhi
+"""
+
+print('Ibu berkata, "Tolong ke Toko Sembako"')
+print('Adli menjawab, "Oke ma, apa yang harus saya lakukan?"')
+print('Ibu menjawab, "Beli satu botol susu, dan jika ada telur beli 6"')
+print("Maka Adli berangkat ke toko")
+print("dan mulai berbelanja")
