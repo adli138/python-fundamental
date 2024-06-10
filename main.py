a802b6cbfe7dfaab824b432df348deb4e270afa2
M.Adli1 <adlimuhammad847@gmail.com>
2024-06-10 15:25:06 +0700

    initial commit

--- main.py
+++ main.py
@@ -5,8 +5,20 @@
 3. Perulangan: Mengulang langkah yang sama berkali kali selama/ sampai kondisi terpenuhi
 """
 
+# Sekuensial
 print('Ibu berkata, "Tolong ke Toko Sembako"')
 print('Adli menjawab, "Oke ma, apa yang harus saya lakukan?"')
 print('Ibu menjawab, "Beli satu botol susu, dan jika ada telur beli 6"')
 print("Maka Adli berangkat ke toko")
 print("dan mulai berbelanja")
+
+# Percabangan
+milk_bottle_count = 123012
+if milk_bottle_count > 0:
+    print('Adli mengecek harganya, dan ternyata uang nya cukup')
+    print('Adli membeli 5 botol susu')
+else:
+    print('Adli gak jadi beli susu')
+
+print('Adli pulang ke rumah')
+print('Menyampaikan belanjaannya kepada ibu')
