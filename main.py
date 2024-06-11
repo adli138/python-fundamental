9210c72493586c04b5f43793f44560a3ddf114e3
M.Adli1 <adlimuhammad847@gmail.com>
2024-06-11 08:30:30 +0700

    initial commit

--- main.py
+++ main.py
@@ -14,11 +14,18 @@ print("dan mulai berbelanja")
 
 # Percabangan
 milk_bottle_count = 123012
+eggs_count = 312897
+print(f'Jumlah Botol Susu {milk_bottle_count} btl')
+print(f'Jumlah Telur {eggs_count} butir')
+
 if milk_bottle_count > 0:
     print('Adli mengecek harganya, dan ternyata uang nya cukup')
-    print('Adli membeli 5 botol susu')
+    if eggs_count > 0:
+        print('Adli membeli 1 botol susu dan 6 butir telur')
+    else:
+        print('Adli membeli 6 botol susu')
 else:
-    print('Adli gak jadi beli susu')
+    print('Adli gak jadi beli susu dan gak jadi beli telur')
 
 print('Adli pulang ke rumah')
 print('Menyampaikan belanjaannya kepada ibu')
