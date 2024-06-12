620834b994100a7fe4a6d1bb54c3efc063527b3b
M.Adli1 <adlimuhammad847@gmail.com>
2024-06-12 16:01:21 +0700

    dasar sintaksis tipe data list

--- dasar_sintaksis_ tipe_data_list.py
+++ dasar_sintaksis_ tipe_data_list.py
@@ -21,3 +21,38 @@ print(daftar_buku[4])
 print('\nTampilkan Seluruh Daftar buku')
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print('\nClear list daftar buku')
+daftar_buku.clear()
+
+daftar_buku = ['Rich Dad Poor Dad', 'Crypto Trading Guide', 'Smart Money Concept', 'Pota Bee']
+daftar_buku.append('Indomaret')
+
+print('\nMengganti Judul buku pertama')
+daftar_buku[0] = 'Estimasi Biaya'
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nMengambil Elemen ke 3')
+buku = daftar_buku.pop(3)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nMenampilkan Elemen yang diambil')
+print(buku)
+
+daftar_buku = ['Rich Dad Poor Dad', 'Crypto Trading Guide', 'Smart Money Concept', 'Pota Bee']
+daftar_buku.append('Indomaret')
+
+print('\nPop')
+daftar_buku.pop()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+daftar_buku = ['Rich Dad Poor Dad', 'Crypto Trading Guide', 'Smart Money Concept', 'Pota Bee']
+daftar_buku.append('Indomaret')
+print('\nPop -2')
+daftar_buku.pop(-2)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
